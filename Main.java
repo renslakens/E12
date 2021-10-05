eee9b38a840d24b2bfa676c1e9ef5e553c2a5e77
Rens Lakens <rens@lakens.org>
2021-10-05 12:24:40 +0200

    added comments to main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        //gaming
+        //Mogelijke waarden zijn: 10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68 en 82, eventueel vermenigvuldigd met machten van 10 (tot 10M)
+        //Kleuren aan getallen toewijzen, ook met de mulitplier
     }
 }
