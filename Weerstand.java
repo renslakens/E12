ed0769482eca3f6ce94f24ef320ae3660e82a950
Rens Lakens <rens@lakens.org>
2021-10-05 20:02:37 +0200

    created weerstand.java

---
+++ Weerstand.java
@@ -0,0 +1,11 @@
+public class Weerstand {
+    private String number;
+
+    public Weerstand(String number) {
+        this.number = number;
+    }
+    
+    public String getNumber() {
+        return number;
+    }
+}
