2246430a4161a5f8f3374db417a2c2edec0ccc92
Rens Lakens <rens@lakens.org>
2021-10-05 16:08:24 +0200

    comments geplaatst over werking uiteindelijke programma en een input gegeven die ook het aantal characters uitrekent

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Main
  */
@@ -5,5 +7,19 @@ public class Main {
     public static void main(String[] args) {
         //Mogelijke waarden zijn: 10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68 en 82, eventueel vermenigvuldigd met machten van 10 (tot 10M)
         //Kleuren aan getallen toewijzen, ook met de mulitplier
+        //Programma moet van eind naar voren lezen voor meer accurate uitkomst
+        
+        int count = 0;
+        Scanner scan = new Scanner(System.in);
+        
+        //Lees hoeveel characters er in de input zitten en print dat uit
+        System.out.println("Give a number: ");
+        String input = scan.nextLine();
+
+        for(int i = 0; i < input.length(); i++) {    
+            if(input.charAt(i) != ' ')    
+                count++;    
+        }
+        System.out.println(count);
     }
 }
