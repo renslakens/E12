db89913e526d0e41f5638070ec49271913d368d7
Rens Lakens <rens@lakens.org>
2021-10-06 12:09:12 +0200

    added getClosestNumber() to Weestand.java

--- Main.java
+++ Main.java
@@ -9,21 +9,6 @@ public class Main {
         //Kleuren aan getallen toewijzen, ook met de mulitplier
         //Programma moet van eind naar voren lezen voor meer accurate uitkomst
         
-        int count = 0;
-        Scanner scan = new Scanner(System.in);
         
-        //Lees hoeveel karakters er in de input zitten en print dat uit
-        System.out.println("Give a number: ");
-        String input = scan.nextLine();
-
-        Weerstand w1 = new Weerstand(input);
-        System.out.println(w1);
-
-        for(int i = 0; i < input.length(); i++) {    
-            if(input.charAt(i) != ' ')    
-                count++;    
-        }
-        
-        System.out.println(count);
     }
 }
